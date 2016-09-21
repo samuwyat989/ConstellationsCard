b13619c17785f0e046ee8480f5dd1243e5679007
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-09-20 19:07:22 -0700

    All starts create but need to be resized and moved. Information added.

--- ConstellationsCard/Form1.Designer.cs
+++ ConstellationsCard/Form1.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // stars
             // 
@@ -36,16 +46,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImage = global::ConstellationsCard.Properties.Resources.night_sky_hd_wallpaper;
-            this.ClientSize = new System.Drawing.Size(755, 479);
+            this.ClientSize = new System.Drawing.Size(609, 479);
+            this.Controls.Add(this.label1);
             this.Name = "stars";
             this.Text = "Constellations Card";
             this.Load += new System.EventHandler(this.stars_Load);
             this.Click += new System.EventHandler(this.stars_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ConstellationsCard/Form1.cs
+++ ConstellationsCard/Form1.cs
@@ -22,7 +22,6 @@ namespace ConstellationsCard
         private void stars_Click(object sender, EventArgs e)
         {
            
-            ///https://stardate.org/astro-guide/pegasus-flying-horse-0
             ///http://www.siwallpaperhd.com/night-sky-hd-wallpaper.html/night-sky-hd-wallpaper-2
             Graphics stars = this.CreateGraphics();
             Pen whitePen = new Pen(Color.White);
@@ -30,61 +29,106 @@ namespace ConstellationsCard
             SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
             Font nameFont = new Font("Times New Roman", 20, FontStyle.Bold);
             Font titleFont = new Font("Times New Roman", 35, FontStyle.Bold);
+            Font desFont = new Font("Courier New", 9);
+            Font tDesFont = new Font("Courier New", 25);
 
-            stars.DrawString("ICS3U First Summative", titleFont, whiteBrush, 140, 190);
-            stars.DrawString("By: Sam Wyatt", nameFont, whiteBrush, 500, 430);
+            stars.DrawString("ICS3U First Summative", titleFont, whiteBrush, 55, 190);
+            stars.DrawString("By: Sam Wyatt", nameFont, whiteBrush, 350, 430);
 
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
 
             stars.Clear(Color.Black);
-            
-            //FillElipse(x, y, width, height)
-            /*stars.FillEllipse(whiteBrush, 100, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 150, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 200, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 150, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 200, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 150, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 350, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 300, 300, 10, 10);
-            stars.FillEllipse(whiteBrush, 300, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 300, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 450, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 400, 300, 10, 10);
-            stars.FillEllipse(whiteBrush, 400, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 400, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 550, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 500, 300, 10, 10);
-            stars.FillEllipse(whiteBrush, 500, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 500, 200, 10, 10);*/
 
             Thread.Sleep(1000);
 
+            stars.DrawString("Constalation - Leo", tDesFont, whiteBrush, 25, 318);
+            stars.DrawString("The Big Dipper is close to Leo and can be identified by the pointer star", desFont, whiteBrush, 30, 350);
+            stars.DrawString("Merak. The constellation becomes visible in the Northern Hemisphere ", desFont, whiteBrush, 30, 375);
+            stars.DrawString("around the spring equinox and is easily identifiable through May.", desFont, whiteBrush, 30, 400);
+            stars.DrawString("Leo represents us in our past lives and, and all tails speak of the", desFont, whiteBrush, 30, 425);
+            stars.DrawString("past and the debts we still have to repay.", desFont, whiteBrush, 30, 450);
+            
+
+            stars.DrawLine(whitePen, 30, 320, 580, 320);
             stars.FillEllipse(yellowBrush, 500, 460, 10, 10);
 
-            stars.FillEllipse(whiteBrush, 50, 50, 10, 10);
-            stars.FillEllipse(whiteBrush, 50, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 250, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 300, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 350, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 400, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 450, 10, 10);
-
-            stars.FillEllipse(whiteBrush, 100, 50, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 100, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 150, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 200, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 250, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 300, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 350, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 400, 10, 10);
-            stars.FillEllipse(whiteBrush, 100, 450, 10, 10);
-
+            stars.FillEllipse(whiteBrush, 50, 50, 10, 10); 
+            stars.FillEllipse(whiteBrush, 50, 100, 8, 8);
+            stars.FillEllipse(whiteBrush, 50, 150, 16, 16);
+            stars.FillEllipse(whiteBrush, 50, 200, 6, 6);
+            stars.FillEllipse(whiteBrush, 50, 250, 7, 7);
+            stars.FillEllipse(whiteBrush, 50, 300, 11, 11);
+            
+            stars.FillEllipse(whiteBrush, 100, 50, 4, 4);
+            stars.FillEllipse(whiteBrush, 100, 100, 8, 8); 
+            stars.FillEllipse(whiteBrush, 100, 150, 3, 3);
+            stars.FillEllipse(whiteBrush, 100, 200, 4, 4);
+            stars.FillEllipse(whiteBrush, 100, 250, 9, 9);
+            stars.FillEllipse(whiteBrush, 100, 300, 6, 6);
+            
+            stars.FillEllipse(whiteBrush, 150, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 150, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 150, 150, 10, 10); 
+            stars.FillEllipse(whiteBrush, 150, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 150, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 150, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 200, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 200, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 200, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 200, 200, 10, 10); 
+            stars.FillEllipse(whiteBrush, 200, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 200, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 250, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 250, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 250, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 250, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 250, 250, 10, 10); 
+            stars.FillEllipse(whiteBrush, 250, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 300, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 300, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 300, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 300, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 300, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 300, 300, 10, 10); 
+            
+            stars.FillEllipse(whiteBrush, 350, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 350, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 350, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 350, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 350, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 350, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 400, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 400, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 400, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 400, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 400, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 400, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 450, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 450, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 450, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 450, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 450, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 450, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 500, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 500, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 500, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 500, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 500, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 500, 300, 10, 10);
+            
+            stars.FillEllipse(whiteBrush, 550, 50, 10, 10);
+            stars.FillEllipse(whiteBrush, 550, 100, 10, 10);
+            stars.FillEllipse(whiteBrush, 550, 150, 10, 10);
+            stars.FillEllipse(whiteBrush, 550, 200, 10, 10);
+            stars.FillEllipse(whiteBrush, 550, 250, 10, 10);
+            stars.FillEllipse(whiteBrush, 550, 300, 10, 10);
+            
         }
 
         private void stars_Load(object sender, EventArgs e)
